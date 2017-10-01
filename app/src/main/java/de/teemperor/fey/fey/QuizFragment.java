abfcd322920434682a0a72205f47c8295f6bb6b3
Raphael Isemann <teemperor@gmail.com>
2017-10-01 12:57:57 +0200

    Renamed LearnFragment to QuizFragment

--- app/src/main/java/de/teemperor/fey/fey/QuizFragment.java
+++ app/src/main/java/de/teemperor/fey/fey/QuizFragment.java
@@ -11,17 +11,15 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
 import java.util.Random;
 
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link LearnFragment#newInstance} factory method to
+ * Use the {@link QuizFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class LearnFragment extends Fragment {
+public class QuizFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -40,7 +38,7 @@ public class LearnFragment extends Fragment {
     private int rightColor = Color.parseColor("#27d500");
     private int wrongColor = Color.parseColor("#d50003");
 
-    public LearnFragment() {
+    public QuizFragment() {
         // Required empty public constructor
         answerOptions = new Button[6];
     }
@@ -60,8 +58,8 @@ public class LearnFragment extends Fragment {
 
 
     // TODO: Rename and change types and number of parameters
-    public static LearnFragment newInstance(String param1, String param2) {
-        LearnFragment fragment = new LearnFragment();
+    public static QuizFragment newInstance(String param1, String param2) {
+        QuizFragment fragment = new QuizFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -106,7 +104,7 @@ public class LearnFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_learn, container, false);
+        View view = inflater.inflate(R.layout.fragment_quiz, container, false);
 
         question = (TextView) view.findViewById(R.id.questionText);
         answerOptions[0] = (Button) view.findViewById(R.id.button1);
